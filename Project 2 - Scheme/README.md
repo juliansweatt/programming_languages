ac75f3f5e6ff961c004a93de937205d689a1e345
Julian Sweatt <20158985+juliansweatt@users.noreply.github.com>
2019-04-03 14:58:57 -0400

    Initialize Project 2
    - Initialized Project 2 Directory
    - Initialized Read Me
    - Copied in Assignment's Provided Files

---
+++ Project 2 - Scheme/README.md
@@ -0,0 +1,17 @@
+# Project 2 - Scheme
+---
+
+## Contents
+* [README.md](./README.md) | _This File_
+    Project details and usage information. 
+* [proj2.scm](./proj2.scm)
+    Main scheme file for the project.
+* [testCases.txt](./testCases.txt)
+    A set of test cases for the scheme project.
+* [Project Specifications](./project2.pdf)
+    Provided project specifications and requirements.
+
+## Usage
+`scheme --load proj2.scm < testCases.txt > sampleOut.txt`
+
+## Remarks
---
+++ Project 2 - Scheme/proj2.scm
@@ -0,0 +1,32 @@
+; Part A: Reverse a list
+(define reverselist
+    (lambda (lst)
+        lst
+    )
+
+)
+
+; ------------------------------------------------------------------------------
+
+; Part B: Insert integer into sorted list
+(define insert
+; TODO: Define function here
+
+)
+
+; ------------------------------------------------------------------------------
+
+; Part C: Check if an item a exists in list
+(define checklist
+; TODO: Define function here
+
+)
+
+; ------------------------------------------------------------------------------
+
+; Part D:
+(define mergelist
+; TODO: Define function here
+
+)
+
